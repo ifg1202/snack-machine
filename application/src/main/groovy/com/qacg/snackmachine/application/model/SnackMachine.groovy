4c8bc842e4312fb8f2668f2d8150eb2a9a9abfff
Ivan Fernandez Guerrero <ifg1202@gmail.com>
2022-03-15 21:53:32 +0100

    feat: Add Money model

--- application/src/main/groovy/com/qacg/snackmachine/application/model/SnackMachine.groovy
+++ application/src/main/groovy/com/qacg/snackmachine/application/model/SnackMachine.groovy
@@ -2,58 +2,20 @@ package com.qacg.snackmachine.application.model
 
 class SnackMachine {
 
-    int oneCentCount
-    int tenCentCount
-    int quarterCount
-    int oneDollarCount
-    int fiveDollarCount
-    int twentyDollarCount
+    Money moneyInside
+    Money moneyInTransaction
 
-    int oneCentCountInTransaction
-    int tenCentCountInTransaction
-    int quarterCountInTransaction
-    int oneDollarCountInTransaction
-    int fiveDollarCountInTransaction
-    int twentyDollarCountInTransaction
-
-    void insertMoney(
-            int oneCentCount,
-            int tenCentCount,
-            int quarterCount,
-            int oneDollarCount,
-            int fiveDollarCount,
-            int twentyDollarCount) {
-        this.oneCentCountInTransaction += oneCentCount
-        this.tenCentCountInTransaction += tenCentCount
-        this.quarterCountInTransaction += quarterCount
-        this.oneDollarCountInTransaction += oneDollarCount
-        this.fiveDollarCountInTransaction += fiveDollarCount
-        this.twentyDollarCountInTransaction += twentyDollarCount
+    void insertMoney(Money money) {
+        moneyInTransaction = Money.add(moneyInTransaction, money)
     }
 
     void returnMoney() {
-        oneCentCountInTransaction = 0
-        tenCentCountInTransaction = 0
-        quarterCountInTransaction = 0
-        oneDollarCountInTransaction = 0
-        fiveDollarCountInTransaction = 0
-        twentyDollarCountInTransaction = 0
+        //moneyInTransaction = 0
     }
 
     void buySnack() {
-        oneCentCount += oneCentCountInTransaction
-        tenCentCount += tenCentCountInTransaction
-        quarterCount += quarterCountInTransaction
-        oneDollarCount += oneDollarCountInTransaction
-        fiveDollarCount += fiveDollarCountInTransaction
-        twentyDollarCount += twentyDollarCountInTransaction
-
-        oneCentCountInTransaction = 0
-        tenCentCountInTransaction = 0
-        quarterCountInTransaction = 0
-        oneDollarCountInTransaction = 0
-        fiveDollarCountInTransaction = 0
-        twentyDollarCountInTransaction = 0
+        moneyInside = Money.add(moneyInside, moneyInTransaction)
+        //moneyInTransaction = 0
     }
 
 }
